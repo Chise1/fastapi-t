da2213351c09a1bd3c4897875a2e5bb857fef26e
chise <chise123@live.com>
2020-12-25 11:29:48 +0800

    update user model

--- fast_tmp/models/__init__.py
+++ fast_tmp/models/__init__.py
@@ -11,6 +11,7 @@ class User(models.Model):
     password = fields.CharField(max_length=200, )
     is_active = fields.BooleanField(default=True, )
     is_superuser = fields.BooleanField(default=False)
+    groups: fields.ManyToManyRelation['Group']
 
     def set_password(self, raw_password: str):
         """
@@ -35,7 +36,8 @@ class User(models.Model):
 class Permission(models.Model):
     label = fields.CharField(max_length=128)
     model = fields.CharField(max_length=128)
-    codename = fields.CharField(max_length=128)
+    codename = fields.CharField(max_length=128, unique=True)
+    groups: fields.ManyToManyRelation['Group']
 
     def __str__(self):
         return self.label
@@ -45,13 +47,33 @@ class Permission(models.Model):
         """
         生成model对应的权限
         """
-        # todo:生成对应默认权限
+        model_name = model.__name__
+        Permission.get_or_create(defaults={
+            "label": "can read " + model_name,
+            "model": model_name,
+            "codename": "can_read_" + model_name
+        })
+        Permission.get_or_create(defaults={
+            "label": "can create " + model_name,
+            "model": model_name,
+            "codename": "can_create_" + model_name
+        })
+        Permission.get_or_create(defaults={
+            "label": "can update " + model_name,
+            "model": model_name,
+            "codename": "can_update_" + model_name
+        })
+        Permission.get_or_create(defaults={
+            "label": "can delete " + model_name,
+            "model": model_name,
+            "codename": "can_delete_" + model_name
+        })
 
 
 class Group(models.Model):
     label = fields.CharField(max_length=50)
-    users = fields.ManyToManyField("models.User")
-    permissions = fields.ManyToManyField("models.Permission")
+    users: fields.ManyToManyRelation[User] = fields.ManyToManyField("models.User")
+    permissions: fields.ManyToManyRelation[Permission] = fields.ManyToManyField("models.Permission")
 
     def __str__(self):
         return self.label
